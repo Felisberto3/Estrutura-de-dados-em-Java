3e162aca7bd56fb7372486bb651c65d9fce3eb7b
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-12-02 13:57:35 +0100

    class No Duplamente Ligada

--- Lista_Duplamente_Ligada/NoD.java
+++ Lista_Duplamente_Ligada/NoD.java
@@ -1,8 +1,47 @@
 package Lista_Duplamente_Ligada;
 
-public class NoD {
+public class NoD<T> {
+
+  private T dado;
+  private NoD<T> anterior;
+  private NoD<T> seguinte;
+
+  public NoD(T dado) {
+    this.dado = dado;
+  }
+
+  public NoD(T dado, NoD<T> anterior, NoD<T> seguinte) {
+    this.dado = dado;
+    this.anterior = anterior;
+    this.seguinte = seguinte;
+  }
+
+  public T getDado() {
+    return dado;
+  }
+
+  public void setDado(T dado) {
+    this.dado = dado;
+  }
+
+  public NoD<T> getAnterior() {
+    return anterior;
+  }
+
+  public void setAnterior(NoD<T> anterior) {
+    this.anterior = anterior;
+  }
+
+  public NoD<T> getSeguinte() {
+    return seguinte;
+  }
+
+  public void setSeguinte(NoD<T> seguinte) {
+    this.seguinte = seguinte;
+  }
 
   // resumo de humanidade
   // Estudar fortemente Probabilidade e Estatistica
   // Resolver o meu pendente de Lista Duplamente Ligada
+
 }
