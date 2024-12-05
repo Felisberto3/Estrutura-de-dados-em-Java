cfae1088eb8cafff40385958d7aa539d5a12b247
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-12-06 00:51:52 +0100

    adicionar

---
+++ Fillas/Filas.java
@@ -0,0 +1,39 @@
+package Fillas;
+
+public class Filas<T> {
+  private No<T> cabeca;
+  private No<T> fundo;
+  private int tamanho = 0;
+
+  public void adicionar(T dado) {
+    No<T> novoNo = new No<T>(dado);
+
+    if (tamanho == 0) {
+      this.cabeca = novoNo;
+    } else {
+      novoNo.setAnterior(fundo);
+      this.fundo.setProximo(novoNo);
+    }
+
+    this.fundo = novoNo;
+    this.tamanho++;
+  }
+
+  public void show() {
+    if (tamanho == 0) {
+      System.out.println("[]");
+      return;
+    }
+    No<T> atual = this.cabeca;
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("[").append(atual.getDado());
+    while (atual.getProximo() != null) {
+      atual = atual.getProximo();
+      builder.append(",").append(atual.getDado());
+    }
+    builder.append("]");
+
+    System.out.println(builder);
+  }
+}
---
+++ Fillas/Testing.java
@@ -0,0 +1,15 @@
+package Fillas;
+
+public class Testing {
+  public static void main(String[] args) {
+    Filas<Integer> a = new Filas<>();
+
+    a.adicionar(1);
+    a.adicionar(2);
+    a.adicionar(2);
+    a.adicionar(3);
+    a.adicionar(3);
+    a.adicionar(6);
+    a.show();
+  }
+}
